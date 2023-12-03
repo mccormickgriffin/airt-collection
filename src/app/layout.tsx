d0869004f5401d63992648f8bf1da197e05f2c22
Griffin McCormick <mccormickgriffin@gmail.com>
2023-12-02 22:13:21 -0500

    added app container element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,29 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from '@/components/header'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import Container from "@/components/container";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'AiRT COLLECTION',
-  description: 'AI Generated Art Gallery',
-}
+  title: "AiRT COLLECTION",
+  description: "AI Generated Art Gallery",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <Container>
+          <Header />
+          {children}
+        </Container>
       </body>
     </html>
-  )
+  );
 }
