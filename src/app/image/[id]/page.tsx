1fef224c1e00ae2af8bfdf00dc6aac2608c031de
Griffin McCormick <mccormickgriffin@gmail.com>
2023-12-06 18:57:22 -0500

    expanded individual image page

--- src/app/image/[id]/page.tsx
+++ src/app/image/[id]/page.tsx
@@ -14,13 +14,18 @@ export default async function EventsPage({ params }: Props) {
   return (
     <main>
       <div className="h-300">
-        <Image
-          src={image.src}
-          alt={image.alt}
-          width={500}
-          height={300}
-        />
+        <Image src={image.src} alt={image.alt} width={500} height={300} />
       </div>
+      <a href={image.src} download="downloaded-image">
+        Download Image
+      </a>
+
+      <p>Source: </p>
+      <p>{image.source}</p>
+      <p>Model: </p>
+      <p>{image.model}</p>
+      <p>Prompt: </p>
+      <p>{image.prompt}</p>
     </main>
   );
 }
